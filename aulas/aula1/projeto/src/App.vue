07bdcec3059c297d44bfc015bc7e2be49d14fc37
Samuelson <samuelsonma@gmail.com>
2021-03-13 05:53:43 -0400

    projeto de testes

---
+++ aulas/aula1/projeto/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+
+    <b-form-input v-model="nome" placeholder="Informe seu nome: ">
+    </b-form-input>
+    <br>
+    {{nome}}
+    <br>
+    <b-button @click="enviar">Enviar</b-button>
+    <b-button v-b-modal="'modal-1'">Abrir modal</b-button>
+
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">{{ nossaVariavel }}</p>
+    </b-modal>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      nossaVariavel: 'Teste',
+      nome: ''
+    }
+  },
+
+  methods: {
+    enviar() {
+      //alert('Nome: ' + this.nome)
+      alert(`Nome: ${this.nome}`)
+    },
+  }
+
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
